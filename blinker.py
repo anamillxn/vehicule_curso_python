95c699ab002530c05a0433568199d6c7f3ec47e4
Ana Millan <anitamillanserrano@gmail.com>
2022-03-04 16:56:22 +0100

    Añado metodo change

---
+++ blinker.py
@@ -0,0 +1,32 @@
+# Blinker position (pos attribute)
+BLINKER_FRONT = 1
+BLINKER_REAR = 2
+
+class Blinker:
+    def __init__(self,pos):
+        self.pos=pos
+        self.deactivate()
+
+    def activate(self):
+        self.activated=True
+ 
+    def deactivate(self):
+        self.activated=False
+ 
+    def get_activated(self):
+        return self.activated
+
+    def change(self):
+        if self.activated==True:
+            self.deactivate()
+        else:
+            self.activate()
+        
+
+    def __str__(self):
+        if self.activated:
+            status = 'A'
+        else:
+            status = 'D'
+        return status
+
